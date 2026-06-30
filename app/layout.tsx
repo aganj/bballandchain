ca5e5158b74a8c28700a95e6a6e5c95989282f4a
Arek <a.ganjalyan@gmail.com>
2026-06-30 05:33:26 +0200

    Added adsense tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4586679406769332"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
